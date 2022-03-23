2fabe26ba3d1f51771057da6e1f180a36e431f57
Gili Shohat <gilish@wix.com>
2022-03-23 09:07:59 +0200

    add eslint and setup an initial server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
+import http from 'http';
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import {config} from './config';
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
 
+const app = express();
 
-console.log('hey')
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+/** Error handling */
+app.use((req, res, next) => {
+  const error = new Error('not found');
+  return res.status(404).json({
+    message: error.message,
+  });
+});
+
+/** Server */
+const httpServer = http.createServer(app);
+const port = config.server.port;
+httpServer.listen(port, () => console.log(`The server is running on port ${port}`));
