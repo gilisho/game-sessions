e4d01acf4f86a9ae11ef312042826efcd8ea3bdc
Gili Shohat <gilish@wix.com>
2022-03-26 01:22:07 +0300

    move logic to controller

--- src/controllers/game-sessions/game-sessions-controller.ts
+++ src/controllers/game-sessions/game-sessions-controller.ts
@@ -1,8 +1,56 @@
 import { RequestHandler } from 'express';
+import { CreateGameSessionRequest, GameSessionRecord } from '../../types';
+import { GradeCalculator } from './grade-calculator';
+import { randomUUID } from 'crypto';
 
-export const gameSessionsController = (): {
+const gameSessions: GameSessionRecord[] = [];
+
+export const gameSessionsController: {
   [operation: string]: RequestHandler;
-} => ({
-  list: (req, res, next) => {},
-  create: (req, res, next) => {},
-});
+} = {
+  list: (req, res, next) => {
+    // returns the list of game sessions (name and id)
+    const gameSessionsList = gameSessions.map((session) => ({
+      id: session.id,
+      game_session_name: session.game_session_name,
+    }));
+    res.send({ gameSessions: gameSessionsList });
+  },
+  get: (req, res, next) => {
+    // returns a game session by id
+    const { id } = req.params;
+    const gameSession = gameSessions.find((session) => session.id === id);
+
+    if (gameSession) {
+      res.send({ gameSession });
+    }
+    res.status(404);
+  },
+  create: (req, res, next) => {
+    //  calculates the measurements' grades and accumulated scores, stores the information in the database and returns the game session id
+    const { gameSession: gameSessionInput } =
+      req.body as CreateGameSessionRequest;
+
+    if (!gameSessionInput) {
+      return res.status(400).send('game session input is missing');
+    }
+
+    const calculator = new GradeCalculator(gameSessionInput.measurements);
+    const {
+      skillScores: { speedScore, accuracyScore },
+    } = calculator.calculate();
+
+    const gameSession: GameSessionRecord = {
+      player_name: gameSessionInput.player_name,
+      game_session_name: gameSessionInput.game_session_name,
+      speedScore,
+      accuracyScore,
+      dateCreated: new Date(),
+      id: randomUUID(),
+    };
+
+    gameSessions.push(gameSession);
+
+    res.send({ id: gameSession.id });
+  },
+};
--- src/routes/game-sessions-router.ts
+++ src/routes/game-sessions-router.ts
@@ -1,60 +1,12 @@
 import express from 'express';
-import { GradeCalculator } from '../controllers/game-sessions/grade-calculator';
-import { CreateGameSessionRequest, GameSessionRecord } from '../types';
-import { randomUUID } from 'crypto';
+import { gameSessionsController } from '../controllers/game-sessions/game-sessions-controller';
 
 const router = express.Router();
 
-// Require controller modules.
+router.get('/', gameSessionsController.list);
 
-const gameSessions: GameSessionRecord[] = [];
+router.get('/:id', gameSessionsController.get);
 
-router.get('/', (req, res, next) => {
-  // returns the list of game sessions (name and id)
-  const gameSessionsList = gameSessions.map((session) => ({
-    id: session.id,
-    game_session_name: session.game_session_name,
-  }));
-  res.send({ gameSessions: gameSessionsList });
-});
-
-router.get('/:id', (req, res, next) => {
-  // returns a game session by id
-  const { id } = req.params;
-  const gameSession = gameSessions.find((session) => session.id === id);
-
-  if (gameSession) {
-    res.send({ gameSession });
-  }
-  res.status(404);
-});
-
-router.post('/', (req, res, next) => {
-  //  calculates the measurements' grades and accumulated scores, stores the information in the database and returns the game session id
-  const { gameSession: gameSessionInput } =
-    req.body as CreateGameSessionRequest;
-
-  if (!gameSessionInput) {
-    return res.status(400).send('game session input is missing');
-  }
-
-  const calculator = new GradeCalculator(gameSessionInput.measurements);
-  const {
-    skillScores: { speedScore, accuracyScore },
-  } = calculator.calculate();
-
-  const gameSession: GameSessionRecord = {
-    player_name: gameSessionInput.player_name,
-    game_session_name: gameSessionInput.game_session_name,
-    speedScore,
-    accuracyScore,
-    dateCreated: new Date(),
-    id: randomUUID(),
-  };
-
-  gameSessions.push(gameSession);
-
-  res.send({ id: gameSession.id });
-});
+router.post('/', gameSessionsController.create);
 
 export { router as gameSessionsRouter };
