7648d2cfdfb5d7c7008db3cdbbb136cd29df2bb9
Gili Shohat <gilish@wix.com>
2022-03-25 15:34:12 +0300

    add type to controller

--- src/controllers/gameSessionsController.ts
+++ src/controllers/gameSessionsController.ts
@@ -1,4 +1,8 @@
-export const gameSessionsController = () => ({
+import { RequestHandler } from 'express';
+
+export const gameSessionsController = (): {
+  [operation: string]: RequestHandler;
+} => ({
   list: (req, res, next) => {},
   create: (req, res, next) => {},
 });
