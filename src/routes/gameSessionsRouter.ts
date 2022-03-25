6b383d2cb16a43d98bcec325d0be8d0d14eabe2c
Gili Shohat <gilish@wix.com>
2022-03-25 12:00:33 +0300

    define basic routes

--- src/routes/gameSessionsRouter.ts
+++ src/routes/gameSessionsRouter.ts
@@ -5,6 +5,17 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   res.send('NOT IMPLEMENTED');
+  // returns the list of game sessions (name and id)
+});
+
+router.get('/:id', (req, res, next) => {
+  // returns a game session by id
+  res.send('NOT IMPLEMENTED - get by id');
+});
+
+router.post('/', (req, res, next) => {
+  //  calculates the measurements' grades and accumulated scores, stores the information in the database and returns the game session id
+  res.send('NOT IMPLEMENTED - create');
 });
 
 export { router as gameSessionsRouter };
