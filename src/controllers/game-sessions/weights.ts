777dee41ede2ca51eaa59da19858b715f516ec50
Gili Shohat <gilish@wix.com>
2022-03-26 12:01:48 +0300

    clean code in weight file

--- src/controllers/game-sessions/weights.ts
+++ src/controllers/game-sessions/weights.ts
@@ -1,9 +1,5 @@
-import { MeasurementType } from '../../types';
-
 export const BOMB_WEIGHT = 0.3;
 export const MOVE_WEIGHT = 0.7;
 export const MISSES_WEIGHT = 0.5;
 export const BODY_WEIGHT = 0.25;
 export const HEADSHOT_WEIGHT = 0.25;
-
-// type Oggject= <T>{[measurementType: T extends MeasurementType]: { averageScore: T }};
